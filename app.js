cc43af2433865eafa47cda0d391833fe29e3f678
deijaparker <deijaparker1@outlook.com>
2024-08-11 22:06:54 -0700

    Add event listener for form submission to prevent default action

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // Cache DOM elements
 const noteForm = document.getElementById('note-form');
 const notesContainer = document.querySelector('#notes-container');
+
+// Event listener for form submission
+noteForm.addEventListener('submit', function(e) {
+    e.preventDefault();
